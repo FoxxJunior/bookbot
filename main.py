9a5e796b1c85b7ebdab6e26783f782098135d75f
FoxxJunior <b.voss@hotmail.com>
2025-10-13 17:02:55 +0200

    Wrote main function and get_book_text

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+# User input translated to path and function called
+def main():
+    path = "books/frankenstein.txt"
+    content = get_book_text(path)
+    print(content)
+
+# Path is read out and content returned as string
+def get_book_text(path):
+    with open(path) as book:
+        string = book.read()
+    return string
+
+main()
